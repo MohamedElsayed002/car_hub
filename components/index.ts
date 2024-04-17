8c0114685c22e89e93552e3f7d90ced16a0b0f8d
MohamedElsayed002 <mohamedelsayed20258@gmail.com>
2024-04-17 17:57:35 +0200

    car hub

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Header from "./Header";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    Header,
+    CustomButton,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    ShowMore,
+    CarDetails
+}
