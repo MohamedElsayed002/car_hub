7c3eebec25f95b23d02cb5519a158a38aef77a3e
MohamedElsayed002 <mohamedelsayed20258@gmail.com>
2024-04-17 18:08:19 +0200

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className='relative overflow-hidden'>
+      <body className='relative '>
         <Header/>
         {children}
         <Footer/>
